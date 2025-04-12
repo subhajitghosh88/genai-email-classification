1b77367f0146d9f2ba719b691535832ea4333a68
Subhajit Ghosh <ghoshsubhajit75@gmail.com>
2025-04-12 18:10:31 +0530

    Initial Project Setup

---
+++ src/main/java/com/subhajit/email/classification/EmailClassificationApplication.java
@@ -0,0 +1,13 @@
+package com.subhajit.email.classification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailClassificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailClassificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/subhajit/email/classification/EmailClassificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhajit.email.classification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailClassificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
